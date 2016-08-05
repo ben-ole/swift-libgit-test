156415daf8450478f9f25d3da93ae4c75b0827a9
Benjamin Müller <elchbenny@googlemail.com>
2016-08-05 13:24:48 +0200

    initial commit

---
+++ Sources/main.swift
@@ -0,0 +1,19 @@
+import Foundation
+import CLibgit2Swift
+
+
+// init git
+Swift.print("libgit init")
+git_libgit2_init()
+
+// query git version
+var major:Int32 = 0
+var minor:Int32 = 0
+var rev:Int32 = 0
+git_libgit2_version(&major, &minor, &rev)
+Swift.print("libgit version: \(major).\(minor).\(rev)")
+
+
+// shutdown git
+git_libgit2_shutdown()
+Swift.print("shut down")
